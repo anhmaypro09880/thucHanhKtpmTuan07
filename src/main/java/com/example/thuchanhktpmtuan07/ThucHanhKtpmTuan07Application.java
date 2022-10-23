e8c46057746a37b11e6dbfcd540720e9478f7b1c
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-10-24 02:22:21 +0700

    Initial commit

---
+++ src/main/java/com/example/thuchanhktpmtuan07/ThucHanhKtpmTuan07Application.java
@@ -0,0 +1,13 @@
+package com.example.thuchanhktpmtuan07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThucHanhKtpmTuan07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThucHanhKtpmTuan07Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thuchanhktpmtuan07/ThucHanhKtpmTuan07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thuchanhktpmtuan07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThucHanhKtpmTuan07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
